3c23e300f30adbceb8badeb7f5b08e6957a6fefc
skippergoroye <oluwatosinadegoroye21@gmail.com>
2025-12-09 10:27:08 +0100

    feat: movies now showing on appwrite

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -9,7 +9,6 @@ import { fetchMovies } from "@/services/api";
 import { updateSearchCount } from "@/services/appwrite";
 
 import SearchBar from "@/components/SearchBar";
-// import MovieDisplayCard from "@/components/MovieCard";
 import MovieCard from "@/components/MovieCard";
 
 const Search = () => {
@@ -46,7 +45,7 @@ const Search = () => {
   }, [searchQuery]);
 
   return (
-    <View>
+    <View  className="flex-1 bg-primary">
       <Image source={images.bg} className="flex-1 absolute w-full z-0" resizeMode="cover" />
 
         <FlatList
